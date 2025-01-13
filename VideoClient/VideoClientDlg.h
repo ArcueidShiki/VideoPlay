b2fc0f86d72a4ede1eee736de256ca10d009630f
Arcueid <52120441+ArcueidShiki@users.noreply.github.com>
2025-01-13 13:54:02 +0800

    Make UI responsive salable resizable
    Signed-off-by: Arcueid <52120441+ArcueidShiki@users.noreply.github.com>

--- VideoClient/VideoClientDlg.h
+++ VideoClient/VideoClientDlg.h
@@ -3,8 +3,8 @@
 //
 
 #pragma once
-
-
+#include <map>
+#include <string>
 // CVideoClientDlg dialog
 class CVideoClientDlg : public CDialogEx
 {
@@ -22,12 +22,27 @@ public:
 
 
 // Implementation
+	
+
 protected:
 	HICON m_hIcon;
-
+	CRect m_lastRect;
+	typedef int CONTROL_ID;
+	std::map<CONTROL_ID, CRect> m_controlRects;
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnDestroy();
+	afx_msg void OnBnClickedBtnStop();
+	CEdit m_video;
+	CSliderCtrl m_pos;
+	CSliderCtrl m_volume;
+	CEdit m_url;
+	afx_msg void OnBnClickedBtnPlay();
+	CButton m_btnPlay;
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
